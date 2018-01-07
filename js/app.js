18757ff48d10bf8b3ec002212135ddd00660da5c
Caroline Forslund <cf222ev@student.lnu.se>
2018-01-07 19:44:37 +0100

    Employee popup is card matching the design for mobile screens

--- js/app.js
+++ js/app.js
@@ -9,17 +9,12 @@ $.ajax({
   },
   success: function(data) {
     const employees = data.results;
-    console.log(employees);
     displayEmployees(employees);
   },
 });
 
 // Create employee card for each employee and add to employees div
 function displayEmployees(employeeArray){
-
-  function capitalizeWords(string) {
-    return string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-  }
 
   const employeesDiv = document.getElementById('employees');
 
@@ -28,11 +23,19 @@ function displayEmployees(employeeArray){
 
     // Variables
     const employee = employeeArray[i];
-    console.log(employee);
+
     const portrait = employee.picture.large;
     const name = capitalizeWords(employee.name.first) + ' ' + capitalizeWords(employee.name.last);
     const email = employee.email;
     const city = capitalizeWords(employee.location.city);
+    const phone = employee.cell;
+    const address = capitalizeWords(employee.location.street) + ', ' + employee.location.postcode;
+    const birthday = employee.dob.substr(5, 2) + '/' + employee.dob.substr(8, 2) + '/' + employee.dob.substr(2, 2);
+
+    // const employee = {
+    //   portrait  : employee.picture.large,
+    //   name      :
+    // }
 
     // Employee Card
     const employeeCard = document.createElement('div');
@@ -56,7 +59,7 @@ function displayEmployees(employeeArray){
     employeeName.innerHTML = name;
     employeeDetails.appendChild(employeeName);
 
-    // Employee email
+    // Employee Email
     const employeeEmail = document.createElement('p');
     employeeEmail.innerHTML = email;
     employeeDetails.appendChild(employeeEmail);
@@ -66,10 +69,99 @@ function displayEmployees(employeeArray){
     employeeCity.innerHTML = city;
     employeeDetails.appendChild(employeeCity);
 
+    // Display more detailed employee info on click
     employeeCard.addEventListener('click', function(){
-      console.log('Hello ' + name);
+      displayEmployee(employee);
     });
-
   }
+}
+
+function displayEmployee(employee){
+  // Variables
+  const portrait = employee.picture.large;
+  const name = capitalizeWords(employee.name.first) + ' ' + capitalizeWords(employee.name.last);
+  const email = employee.email;
+  const city = capitalizeWords(employee.location.city);
+  const phone = employee.cell;
+  const address = capitalizeWords(employee.location.street) + ', ' + employee.location.city + ' ' + employee.location.postcode;
+  const birthday = employee.dob.substr(5, 2) + '/' + employee.dob.substr(8, 2) + '/' + employee.dob.substr(2, 2);
+
+  const main = document.getElementById('main');
+
+  //Create and display overlay div
+  const overlay = document.createElement('div');
+  overlay.id = 'overlay';
+  main.appendChild(overlay);
+
+  overlay.addEventListener('click', function(){
+    overlay.remove();
+  });
+
+  //Create and display the employee detailed card
+  const hr = document.createElement('hr');
+  const employeeCard = document.createElement('div');
+  employeeCard.className = 'employee-card';
+  overlay.appendChild(employeeCard);
+
+  // Employee Portrait
+  const employeePortrait = document.createElement('img');
+  employeePortrait.className = 'portrait';
+  employeePortrait.src = portrait;
+  employeePortrait.alt = 'Portrait of ' + name;
+  employeeCard.appendChild(employeePortrait);
+
+  // Employee Name
+  const employeeName = document.createElement('h2');
+  employeeName.innerHTML = name;
+  employeeCard.appendChild(employeeName);
+
+  // Employee Email
+  const employeeEmail = document.createElement('p');
+  employeeEmail.innerHTML = email;
+  employeeCard.appendChild(employeeEmail);
+
+  // Employee City
+  const employeeCity = document.createElement('p');
+  employeeCity.innerHTML = city;
+  employeeCard.appendChild(employeeCity);
+
+  // Employee Phone
+  const employeePhone = document.createElement('p');
+  employeePhone.innerHTML = phone;
+  employeeCard.appendChild(employeePhone);
+
+  // Employee Address
+  const employeeAddress = document.createElement('p');
+  employeeAddress.innerHTML = address;
+  employeeCard.appendChild(employeeAddress);
+
+  // Employee Birthday
+  const employeeBirthday = document.createElement('p');
+  employeeBirthday.innerHTML = birthday;
+  employeeCard.appendChild(employeeBirthday);
+
+  //Create close button
+  const close = document.createElement('span');
+  close.className = 'close-card';
+  close.innerHTML = '&times;';
+  close.addEventListener('click', function(){
+    overlay.remove();
+  });
+  employeeCard.appendChild(close);
+
+  //Create back and forvard buttons
 
 }
+
+// =============================================================================
+// HELPER FUNCTIONS
+// =============================================================================
+function capitalizeWords(string) {
+  return string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+// function createAndAppendNode(element, attribute, setting, parentNode){
+//   const newNode = document.createElement(`'#{element}'`);
+//   newNode.attribute = setting;
+//   parentNode.appendChild(newNode);
+// }
